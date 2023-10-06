959dcf9a9f19ca8dd355daacf9b4c7c897c7fb78
akashdomadiya <136041940+akashdomadiya@users.noreply.github.com>
2023-10-06 11:53:13 +0530

    fpart

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../../Components/Layouts/Layout';
+import Section1 from './Section1';
+import '../../Styles/HomeStyle.css';
 
 function Home() {
   return (
   <>
     <Layout>
-      <div>Home</div>
+        <Section1/>
     </Layout>
   </>
   )
